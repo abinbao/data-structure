c645932ffb781936ea3db615e9e711df96b1bfdf
baiyaguang <301070179@qq.com>
2019-04-28 22:39:17 +0800

    treenode

---
+++ src/main/java/com/beng/leetcode/treenode/TreeNode.java
@@ -0,0 +1,14 @@
+package com.beng.leetcode.treenode;
+
+/**
+ * @author apple
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
