6f101ba5e9db3f15c61039b3d9e6397844366930
baiyaguang <301070179@qq.com>
2019-05-15 15:49:19 +0800

    找到小镇的法官

---
+++ src/main/java/com/beng/leetcode/graph/FindJudge.java
@@ -0,0 +1,40 @@
+package com.beng.leetcode.graph;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+/**
+ * 找到小镇的法官
+ * 
+ * @link leetcode 997
+ * @author apple
+ */
+public class FindJudge {
+
+    public static int findJudge(int N, int[][] trust) {
+        Map<Integer, Integer> output = new HashMap<>();
+        Map<Integer, Integer> input = new HashMap<>();
+
+        for (int n = 1; n <= N; ++n) {
+            output.put(n, 0);
+            input.put(n, 0);
+        }
+        for (int i = 0; i < trust.length; ++i) {
+            int[] arr = trust[i];
+            output.put(arr[1], output.get(arr[1]) + 1);
+            input.put(arr[0], input.get(arr[0]) - 1);
+        }
+        for (Entry<Integer, Integer> entry : output.entrySet()) {
+            int key = entry.getKey();
+            if (entry.getValue() == N - 1 && input.get(key) == 0)
+                return key;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[][] trust = new int[][] { { 1, 3 }, { 2, 3 }, { 3, 1 } };
+        System.out.println(findJudge(3, trust));
+    }
+}
